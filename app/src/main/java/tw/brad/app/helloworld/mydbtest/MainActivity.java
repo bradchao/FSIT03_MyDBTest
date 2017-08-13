bc6600cd0d377f4000d9a446c5befbba7336c4c1
Chien <chien@gmail.com>
2017-08-13 13:55:03 +0800

    v2

--- app/src/main/java/tw/brad/app/helloworld/mydbtest/MainActivity.java
+++ app/src/main/java/tw/brad/app/helloworld/mydbtest/MainActivity.java
@@ -2,6 +2,7 @@ package tw.brad.app.helloworld.mydbtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void insert(View view){
+
+    }
+    public void delete(View view){
+
+    }
+    public void update(View view){
+
+    }
+    public void query(View view){
+
     }
 }
