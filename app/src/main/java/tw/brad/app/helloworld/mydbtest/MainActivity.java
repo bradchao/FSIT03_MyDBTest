8c3e356fa4e71d3a2750c858c6422a3649c66bbe
Chien <chien@gmail.com>
2017-08-13 14:40:35 +0800

    v3

--- app/src/main/java/tw/brad/app/helloworld/mydbtest/MainActivity.java
+++ app/src/main/java/tw/brad/app/helloworld/mydbtest/MainActivity.java
@@ -1,15 +1,24 @@
 package tw.brad.app.helloworld.mydbtest;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private MyDBHelper myDBHelper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDBHelper = new MyDBHelper(this, "brad", null, 1);
+        db = myDBHelper.getReadableDatabase();
+
     }
 
     public void insert(View view){
@@ -22,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void query(View view){
-
+        // select * from cust
+        Cursor cursor = db.query("cust",null,null,null,null,null,null);
+        while (cursor.moveToNext()){
+            String id = cursor.getString(0);
+            String cname = cursor.getString(1);
+            String tel = cursor.getString(2);
+            String birthday = cursor.getString(3);
+            Log.i("brad", id+":"+cname+":"+tel+":"+birthday);
+        }
     }
 }
